1c46afb512ab496a7f37ce3b73549fd25e5f247c
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-09-03 08:56:00 +0200

    Exercising functions for Academy...

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 "use strict";
+
+function calculateRectArea(a, b) {
+  return a * b;
+}
+
+var result = calculateRectArea(5, 10);
+
+console.log(result);
+
+function sayHello(name) {
+  console.log("Hello " + name);
+}
+
+sayHello("Ben");
+
+var calculateRectArea = new Function("a", "b", "return a*b");
+var result = calculateRectArea(5, 10);
+console.log(result);
+
+function sum() {
+  let returnValue = 0;
+
+  for (let i = 0; i < arguments.length; i++) {
+    returnValue += arguments[i];
+  }
+
+  return returnValue;
+}
+
+let sumResult = sum(10, 15, 15);
+
+console.log(sumResult);
