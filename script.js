a066ec1410cda35161dc93df0b99ff5f26b86ece
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-09-18 19:31:51 +0200

    Timing and callback functions...

--- script.js
+++ script.js
@@ -3,18 +3,9 @@
 var myButton = document.getElementById("my-button");
 
 myButton.addEventListener("click", function () {
-  //time-intensive operation start
+  alert("Hello from modal window.");
 
-  var data = [];
-
-  for (let i = 0; i < 50000; i++) {
-    let random = Math.random().toString().split("0");
-    data = data.concat(random);
-  }
-
-  console.log(data.length);
-
-  //time-intenscive operation end
-
-  console.log("Hello from statement after time-intensive operation.");
+  console.log("Hello from statement after alert() method.");
 });
+
+console.log("Hello from some code after addEventListener() method.");
